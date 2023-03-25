1ee38e40c0d6539b9d039931ce744a6026928767
erickoo <ericcool.1129@gmail.com>
2023-03-25 15:07:30 +0800

    initialize project

---
+++ loan-management-system-backend/src/test/java/eric/koo/loanmanagementsystem/LoanManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package eric.koo.loanmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
