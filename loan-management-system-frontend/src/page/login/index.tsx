29c3994f24239ee1ac988d68881df2212c66ed22
erickoo <ericcool.1129@gmail.com>
2023-03-29 03:21:03 +0800

    add toast

--- loan-management-system-frontend/src/page/login/index.tsx
+++ loan-management-system-frontend/src/page/login/index.tsx
@@ -1,4 +1,5 @@
 import { Button } from "react-bootstrap";
+import { toast } from "react-toastify";
 import useTitle from "../../hook/useTitle";
 import useStore from "../../store/useStore";
 import { Role } from "../../store/interface/IAuthSlice";
@@ -7,7 +8,11 @@ const LoginPage = () => {
     useTitle("Login");
     const login = useStore((state) => state.login);
 
-    return <Button onClick={() => login("admin", "admin", Role.APPLICANT)}>Login</Button>;
+    return (
+        <Button onClick={() => login("admin", "admin", Role.APPLICANT).catch((error) => toast.error(error))}>
+            Login
+        </Button>
+    );
 };
 
 export default LoginPage;
