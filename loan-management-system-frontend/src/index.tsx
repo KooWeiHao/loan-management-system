be48fd6bb2daf9c67a49cfa84c06990cf7f7227c
erickoo <ericcool.1129@gmail.com>
2023-03-28 23:02:37 +0800

    minor update

--- loan-management-system-frontend/src/index.tsx
+++ loan-management-system-frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import "./app.scss";
+import "./App.scss";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
