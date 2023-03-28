5071edaf6f3ce75a080f48059f00e28f47bfc9fb
erickoo <eric.koo@orangekloud.com>
2023-03-28 17:55:20 +0800

    implement cors configuration

--- loan-management-system-backend/src/main/java/eric/koo/loan/management/system/config/WebMvcConfiguration.java
+++ loan-management-system-backend/src/main/java/eric/koo/loan/management/system/config/WebMvcConfiguration.java
@@ -6,8 +6,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @Configuration
 class WebMvcConfiguration implements WebMvcConfigurer {
@@ -16,6 +19,21 @@ class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Value("${loan.management.system.api.path.prefix}")
     private String apiPathPrefix;
+
+    @Value("${loan.management.system.allowed.origins}")
+    private String[] allowedOrigins;
+
+    @Value("${loan.management.system.allowed.methods}")
+    private String[] allowedMethods;
+
+    @Value("${loan.management.system.allowed.headers}")
+    private String[] allowedHeaders;
+
+    @Value("${loan.management.system.max.age}")
+    private Long maxAge;
+
+    @Value("${loan.management.system.allow.credentials}")
+    private boolean allowCredentials;
 
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
@@ -26,5 +44,21 @@ class WebMvcConfiguration implements WebMvcConfigurer {
                 apiPathPrefix,
                 HandlerTypePredicate.forBasePackageClass(Controller.class)
         );
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        logger.info("Allowed Origins - {}", Arrays.toString(allowedOrigins));
+        logger.info("Allowed Methods - {}", Arrays.toString(allowedMethods));
+        logger.info("Allowed Headers - {}", Arrays.toString(allowedHeaders));
+        logger.info("Max Age - {}", maxAge);
+        logger.info("Allow Credentials - {}", allowCredentials);
+
+        registry.addMapping("/**")
+                .allowedOriginPatterns(allowedOrigins)
+                .allowedMethods(allowedMethods)
+                .allowedHeaders(allowedHeaders)
+                .maxAge(maxAge)
+                .allowCredentials(allowCredentials);
     }
 }
