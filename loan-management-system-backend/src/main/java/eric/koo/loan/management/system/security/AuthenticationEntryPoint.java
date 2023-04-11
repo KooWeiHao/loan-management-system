e8bfedc93934af946d9011f45a287498479ec323
Eric Koo Wei Hao <47848493+KooWeiHao@users.noreply.github.com>
2023-04-11 09:42:45 +0800

    Update AuthenticationEntryPoint.java

--- loan-management-system-backend/src/main/java/eric/koo/loan/management/system/security/AuthenticationEntryPoint.java
+++ loan-management-system-backend/src/main/java/eric/koo/loan/management/system/security/AuthenticationEntryPoint.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+// to handle unauthenticated requests & return an HTTP response
 @Component
 class AuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
 
